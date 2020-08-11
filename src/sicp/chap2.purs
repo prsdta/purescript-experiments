d10db979100d84c9fc4aa994661e87a7f7c3ff06
prsdta -Aubin <uhryks@gmail.com>
2020-08-12 00:15:19 +0900

    feat: Add all the base functions
    Also reformatted with purty

--- src/sicp/chap2.purs
+++ src/sicp/chap2.purs
@@ -7,7 +7,15 @@ import Prelude
 --
 -- But that seems to create a lot of syntax noise, and since we will not reuse
 -- most instances, this is not worth it?
-data Rat n d = Rat Int Int
+data Rat n d
+  = Rat Int Int
+
+makeRat :: Int -> Int -> Rat Int Int
+makeRat n d =
+  let
+    g = gcd n d
+  in
+    Rat (n / g) (d / g)
 
 numer :: Rat Int Int -> Int
 numer (Rat n d) = n
@@ -17,3 +25,21 @@ denom (Rat n d) = d
 
 instance showRat :: Show (Rat Int Int) where
   show (Rat n d) = show n <> "/" <> show d
+
+addRat :: Rat Int Int -> Rat Int Int -> Rat Int Int
+addRat x y =
+  makeRat
+    ((numer x * denom y) + (numer y * denom x))
+    (denom x * denom y)
+
+subRat :: Rat Int Int -> Rat Int Int -> Rat Int Int
+subRat x y =
+  makeRat
+    ((numer x * denom y) - (numer y * denom x))
+    (denom x * denom y)
+
+mulRat :: Rat Int Int -> Rat Int Int -> Rat Int Int
+mulRat x y = makeRat (numer x * numer y) (denom x * denom y)
+
+divRat :: Rat Int Int -> Rat Int Int -> Rat Int Int
+divRat x y = makeRat (numer x * denom y) (denom x * numer y)
